5fdc21535b2bc52af2a5f88a2aa138a9cac7faf8
Nguyen Nguyen <nasongnguyen@gmail.com>
2025-04-19 14:14:29 +0200

    create PieChart

---
+++ src/components/charts/PieChart.jsx
@@ -0,0 +1,24 @@
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export default function PieChart({ transactions }) {
+  const expenseData = transactions.filter(t => t.type === 'expense');
+
+  const categorySums = expenseData.reduce((acc, t) => {
+    acc[t.category] = (acc[t.category] || 0) + Math.abs(t.amount);
+    return acc;
+  }, {});
+
+  const data = {
+    labels: Object.keys(categorySums),
+    datasets: [{
+      label: 'Expenses by Category',
+      data: Object.values(categorySums),
+      backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#6A5ACD'],
+    }],
+  };
+
+  return <Pie data={data} />;
+}
