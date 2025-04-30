73c37fa8d26a81e803ab6acd473aeaa7a374825b
Nguyen Nguyen <nasongnguyen@gmail.com>
2025-04-30 22:32:26 +0300

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,10 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.vitest, // ✅ Add this line
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
